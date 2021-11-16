d85060d91e9b8c0fee217fae12eacbe6e7eb9d9e
Kate McColl <kate@katemccollart.com>
2021-11-16 14:14:11 +1300

    add: Apollo Server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./routes');
+
 const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware,
 });
 
 server.applyMiddleware({ app });
@@ -23,7 +26,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
@@ -32,7 +34,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
