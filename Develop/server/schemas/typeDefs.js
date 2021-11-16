0786c2544a0e1a8be83c4ed30f7d7ec61efa5c06
Kate McColl <kate@katemccollart.com>
2021-11-16 14:15:57 +1300

    fix: made adjustments to Query and Mutation; add: tabs for readability

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -2,41 +2,47 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type Book {
-    _id: ID
-    authors: [String]!
-    description: String
-    bookId: String
-    image: String
-    link: String
-    title: String
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
     }
     
     type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-    savedBooks: [Book]!
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    
+    input savedBooks {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
     }
     
     type Auth {
-    token: ID!
-    user: User
+        token: ID!
+        user: User
     }
     
     type Query {
-    users: [User]
-    user(username: String@): User
-    savedBooks(username: String): [Book]
-    book(bookId: ID!): Book
-    me: User
+        me: User
     }
     
     type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    saveBook(savedBooks: String): User
-    removeBook(bookId: String): User
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBooks(input: savedBooks!): User
+        removeBook(bookId: String!): User
     }
 `
 
